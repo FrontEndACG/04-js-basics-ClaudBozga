c45025015f02c939d5e2cce1ebd0848730a0c736
Claud Bozga <claudia.bozga@gmail.com>
2023-10-23 11:59:16 +0300

    rezolvat si 10

--- 10_Checking_for_substrings/ciordeala.js
+++ 10_Checking_for_substrings/ciordeala.js
@@ -1,29 +1,28 @@
 // JavaScript program to check if a string is
 // substring of other.
- 
+
 // Returns true if s1 is substring of s2
 function isSubstring(s1, s2)
 {
-    var M = s1.length;
-    var N = s2.length;
- 
+    var M = s1.length; //substr
+    var N = s2.length; //str
+
     /* A loop to slide pat[] one by one */
     for (var i = 0; i <= N - M; i++) {
         var j;
- 
+
         /* For current index i, check for
  pattern match */
         for (j = 0; j < M; j++)
             if (s2[i + j] != s1[j])
                 break;
- 
+
         if (j == M)
             return i;
     }
- 
     return -1;
 }
- 
+
 /* Driver code */
 var s1 = "for";
 var s2 = "geeksforgeeks";
